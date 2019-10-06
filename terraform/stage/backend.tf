36f80f3d7452b51254315603ef0f89c39298bda2
ivango812 <ivan@newbusinesslogic.ru>
2019-10-06 17:44:08 +0300

    Changed terraform version to 0.12.18

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.9"
+  required_version = "0.12.18"
   backend "gcs" {
     bucket  = "storage-tfstate"
     prefix  = "terraform/state-stage"
