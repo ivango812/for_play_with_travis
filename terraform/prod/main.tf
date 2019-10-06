71fff3b364718386506faa433f9dd468a6c90a57
ivango812 <ivan@newbusinesslogic.ru>
2019-10-06 17:37:43 +0300

    Changed 'prod' on var.env_name in prod/main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -10,7 +10,7 @@ module "app" {
   private_key_path = var.private_key_path
   zone            = var.zone
   app_disk_image  = var.app_disk_image
-  env_name        = "prod"
+  env_name        = var.env_name
   database_url    = "${module.db.mongo_ip}:${module.db.mongo_port}"
 }
 
@@ -20,11 +20,11 @@ module "db" {
   private_key_path = var.private_key_path
   zone            = var.zone
   db_disk_image   = var.db_disk_image
-  env_name        = "prod"
+  env_name        = var.env_name 
 }
 
 module "vpc" {
   source        = "../modules/vpc"
   source_ranges = ["95.161.223.68/32"]
-  env_name      = "prod"
+  env_name      = var.env_name 
 }
