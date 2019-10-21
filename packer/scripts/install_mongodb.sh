87643c37e8eea3e714fb0a7e326c6e59ddf6bcd0
ivango812 <ivan@newbusinesslogic.ru>
2019-10-21 03:51:24 +0300

    Changed packer provisioners to Ansible

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -3,5 +3,5 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D68FA50FEA3129
 bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 apt update
 apt install -y mongodb-org
-systemctl start mongod
 systemctl enable mongod
+systemctl start mongod
