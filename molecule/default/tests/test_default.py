46f510c3377ad44f47f7b85c4517a51910cb1eb4
ivango812 <ivan@newbusinesslogic.ru>
2019-10-29 23:26:14 +0300

    Fixed test_default.py lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,11 +13,13 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
+
 # check if configuration file contains the required line
 def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
 
 def test_mongo_is_listening(host):
     assert host.socket("tcp://0.0.0.0:27017").is_listening
